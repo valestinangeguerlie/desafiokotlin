16159acdb0a11b79bbb79c0fdaa230dcc3e59fee
valestinangeguerlie <valestinangie@gmail.com>
2022-06-17 22:28:13 -0300

    commit

---
+++ app/src/main/java/com/example/desafiokotlin/Pantalla_de_sugerencia2.kt
@@ -0,0 +1,11 @@
+package com.example.desafiokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pantalla_de_sugerencia2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_de_sugerencia2)
+    }
+}
